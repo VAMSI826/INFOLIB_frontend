dd5f9647190cc4216d1bf1bce29e26cdf838966b
Vamsi826 <vamsim628@gmail.com>
2024-07-07 15:39:19 +0530

    IJO

--- src/Component/Student-module/Profile/profile.jsx
+++ src/Component/Student-module/Profile/profile.jsx
@@ -34,51 +34,75 @@ export default function Profile() {
         <section style={{ backgroundColor: "#eee" }}>
           <div class="container profile py-5 mt-5">
             <div class="col">
-              <div class="col-lg-8">
-                <div class="card mb-4">
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col-sm-3">
-                        <p class="mb-0">Full Name</p>
+              <div class="col-lg-4 mb-4">
+                <div class="card mb-4 ">
+                  <div class="card-body text-center">
+                    {studata.gender === "Male" ? (
+                      <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                        alt="avatar"
+                        class="rounded-circle img-fluid mt-3"
+                        style={{ width: "150px" }}
+                      />
+                    ) : (
+                      <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1.webp"
+                        alt="avatar"
+                        class="rounded-circle img-fluid mt-3"
+                        style={{ width: "150px" }}
+                      />
+                    )}
+                    <h5 class="my-3">{studata.name}</h5>
+                    <p class="text-muted mb-2">{studata.uucms}</p>
+                    <p class="text-muted mb-4">{studata.userid}</p>
+                  </div>
+                </div>
+                <div class="col-lg-8">
+                  <div class="card mb-4">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-sm-3">
+                          <p class="mb-0">Full Name</p>
+                        </div>
+                        <div class="col-sm-9">
+                          <p class="text-muted mb-0">{studata.name}</p>
+                        </div>
                       </div>
-                      <div class="col-sm-9">
-                        <p class="text-muted mb-0">{studata.name}</p>
+                      <hr />
+                      <div class="row">
+                        <div class="col-sm-3">
+                          <p class="mb-0">Course</p>
+                        </div>
+                        <div class="col-sm-9">
+                          <p class="text-muted mb-0">{studata.course}</p>
+                        </div>
                       </div>
-                    </div>
-                    <hr />
-                    <div class="row">
-                      <div class="col-sm-3">
-                        <p class="mb-0">Course</p>
+                      <hr />
+                      <div class="row">
+                        <div class="col-sm-3">
+                          <p class="mb-0">Semester</p>
+                        </div>
+                        <div class="col-sm-9">
+                          <p class="text-muted mb-0">{studata.semester}</p>
+                        </div>
                       </div>
-                      <div class="col-sm-9">
-                        <p class="text-muted mb-0">{studata.course}</p>
+                      <hr />
+                      <div class="row">
+                        <div class="col-sm-3">
+                          <p class="mb-0">Email ID</p>
+                        </div>
+                        <div class="col-sm-9">
+                          <p class="text-muted mb-0">{studata.email}</p>
+                        </div>
                       </div>
-                    </div>
-                    <hr />
-                    <div class="row">
-                      <div class="col-sm-3">
-                        <p class="mb-0">Semester</p>
-                      </div>
-                      <div class="col-sm-9">
-                        <p class="text-muted mb-0">{studata.semester}</p>
-                      </div>
-                    </div>
-                    <hr />
-                    <div class="row">
-                      <div class="col-sm-3">
-                        <p class="mb-0">Email ID</p>
-                      </div>
-                      <div class="col-sm-9">
-                        <p class="text-muted mb-0">{studata.email}</p>
-                      </div>
-                    </div>
-                    <hr />
-                    <div class="row">
-                      <div class="col-sm-3">
-                        <p class="mb-0">Phone Number</p>
-                      </div>
-                      <div class="col-sm-9">
-                        <p class="text-muted mb-0">{studata.phone}</p>
+                      <hr />
+                      <div class="row">
+                        <div class="col-sm-3">
+                          <p class="mb-0">Phone Number</p>
+                        </div>
+                        <div class="col-sm-9">
+                          <p class="text-muted mb-0">{studata.phone}</p>
+                        </div>
                       </div>
                     </div>
                   </div>
